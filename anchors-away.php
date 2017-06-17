97b21b5b2f72423c5dafc16c5df70c640366fd9a
gadams82 <gary@wormholewebworks.com>
2017-06-17 08:20:17 -0700

    Update anchors-away.php

--- anchors-away.php
+++ anchors-away.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
   ___                                               ___                                 
  -   -_,            ,,                             -   -_,                           /\ 
 (  ~/||             ||                            (  ~/||  ;        _                \/ 
@@ -35,7 +35,6 @@
 	##,. .#...(       '   .c  c .c  c  c.    '..      ;; ../ 
 	%%#%;,..##.\_                           ,;###;,. ;;.:##;,.    
 	%%%%########%%%%;,.....,;%%%%%%;,.....,;%%%%%%%%%%%%%%%%%%%%............              
-version 1.0 
 */
 
 
@@ -48,9 +47,6 @@ function anchorsAway($url, $plunder, $depth = 5)
     if (isset($seen[$url]) || $depth === 0) {
         return;
     }
-	
-	
-	
 	
 	//log seen pages 
     $seen[$url] = true;
@@ -109,50 +105,138 @@ function plunder($plunder, $resources){
 
 	$dom = $resources['dom'];
 	$url = $resources['url'];
+	$finder = new DomXPath($dom);
+	$booty = array();
 	//get title tags
-	if(isset($plunder['title'])){
-		$titles = $dom->getElementsByTagName("title");
-		$title = $titles->item(0)->textContent;		
+	if(isset($plunder['titles'])){
+		$titles = $dom->getElementsByTagName("title");	
+		if(isset($titles->item(0)->textContent)){
+			$title = $titles->item(0)->textContent;	
+		}
 	}	
 	if(isset($plunder['metas'])){
 		//get meta tags
-		$metas = get_meta_tags($url);
+		$metas = @get_meta_tags($url);
 	}
-	echo "
-	array(
-			
-		['url'] => '".basename($url)."',";
-		echo "\r\n";
-		if(isset($title)){
-			echo "\t\t['title'] => '".$title."',";
-			echo "\r\n";
-		}
-		if(isset($metas['description'])){
-			echo "\t\t['description'] => '".$metas['description']."',";
-			echo "\r\n";
-		}
-		if(isset($metas['keywords'])){
-			echo "\t\t['keywords'] => '".$metas['keywords']."',";
-			echo "\r\n";
-		}
-		echo "\r\n";
-		echo "\t\t),";
+
+	if(isset($plunder['drop dashes'])){
+		$url_key = str_replace('-'," ",basename($url));
+	}
+	else{
+		$url_key = basename($url);
+	}
+	$booty[$url_key] = array();
 	
+	
+	if(isset($title)){
+		$booty[$url_key]['title'] = addslashes($title);
+	}
+	if(isset($metas['description'])){
+		$booty[$url_key]['description'] = addslashes($metas['description']);
+	}
+	if(isset($metas['keywords'])){
+		$booty[$url_key]['keywords'] = addslashes($metas['keywords']);
+	}
+	
+	//scrape images
+	if(isset($plunder['img'])){
+		$booty[$url_key]['imgs'] = array();
+		//get images 
+		$images = $dom->getElementsByTagName("img");
+		
+		//iterate through and get paths
+		foreach($images as $image){
+			$image_paths[]=urldecode($image->getAttribute('src'));
+		}
+		//filter out all null, false and empty values
+		$image_paths = array_filter($image_paths,'strlen');
+		//reset keys after removing empties
+		$image_paths = array_values($image_paths);
+		//write our array
+		foreach($image_paths as $key => $image_path){
+			$booty[$url_key]['imgs'][$key] = $image_path;
+		}
+
+	}
+	
+
+		if(isset($plunder['elements by id'])){
+			
+			$element = $dom->getElementById($plunder['elements by id'])->textContent;
+			$booty[$url_key]['text content'] = trim($element);
+
+		}
+		if(isset($plunder['img xpath'])){
+			
+			$xpath = $plunder['img xpath'];
+
+			$content = $finder->query($xpath);
+			
+			foreach ($content as $img) {
+				$booty[$url_key]['xpath imgs'][] = $img->getAttribute('src');
+			}
+		}
+		if(isset($plunder['description xpath'])){
+			
+			$xpath = $plunder['description xpath'];
+			
+			$content = $finder->query($xpath);
+			
+			foreach ($content as $desc) {
+				$booty[$url_key]['xpath description'][] = $desc->textContent;
+			}
+		}
+		if(isset($plunder['children xpath'])){
+			
+			$xpath = $plunder['children xpath'];
+			
+			$content = $finder->query($xpath);
+			
+
+			foreach ($content as $desc) {
+				
+				
+				var_dump(DOMinnerHTML($desc));
+				
+				
+				$booty[$url_key]['xpath html'][] = $desc;
+			}
+		}
+		
+		echo '<pre>';
+		var_dump($booty); 
+		echo '</pre>';
+
 }
 
+function DOMinnerHTML(DOMNode $element) 
+{ 
+    $innerHTML = ""; 
+    $children  = $element->childNodes;
 
+    foreach ($children as $child) 
+    { 
+        $innerHTML .= $element->ownerDocument->saveHTML($child);
+    }
+
+    return $innerHTML; 
+} 
 
 
 $myPlunder = array(
-
-	'titles' => true,
-	'metas' => true,
-	
+	'img' => 1,
+	//'elements by id' => 'partLongDescription',
+	//'img xpath' => "//div[@id='ie8Image']/img",
+	//'description xpath' => "//div[@class='wpb_wrapper']",
+	//'children xpath' => "//div[@class='wpb_wrapper']",
+	//'metas' => true,
+	//'titles' => true,
+	//'drop dashes' => true,
 	
 );
 
 
-anchorsAway('http://kinneynursery.com', $myPlunder, 2);
+anchorsAway('http://tappinroots.com/products/all-stages/', $myPlunder, 2);
 
 
 ?>
